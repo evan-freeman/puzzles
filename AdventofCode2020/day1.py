02e006e38301fe7415b8bbcea5a48d431f151dca
evan-freeman <59422750+evan-freeman@users.noreply.github.com>
2020-12-02 05:36:01 -0800

    day 2 and some reformatting

--- AdventofCode2020/day1.py
+++ AdventofCode2020/day1.py
@@ -1,3 +1,29 @@
+def puzzle1(lst):
+    compliments = {}
+    for x in lst:
+        if x not in compliments:
+            y = 2020 - x
+            compliments[y] = x
+        else:
+            return x * compliments[x]
+
+
+def puzzle1maybefaster(lst):
+    compliments = {}
+    for x in lst:
+        if x not in compliments:
+            compliments[2020 - x] = x
+        else:
+            return x * compliments[x]
+
+
+def puzzle1slow(lst):
+    for x in lst:
+        for y in lst:
+            if x != y and x + y == 2020:
+                return x * y
+
+
 def puzzle2(lst):
     comps1 = {}
     comps2 = {}
@@ -35,9 +61,15 @@ def puzzle2slow(lst, target=2020):
 
 
 if __name__ == "__main__":
+    sample_input = [1721, 979, 366, 299, 675, 1456]
     input1 = open("input1.txt", "r").read()
     input1_list = [int(x) for x in input1.split("\n") if x]
-    sample_input = [1721, 979, 366, 299, 675, 1456]
+
+    print(puzzle1(sample_input))
+    print(puzzle1slow(sample_input))
+
+    print(puzzle1(input1_list))
+    print(puzzle1slow(input1_list))
 
     print(puzzle2(sample_input))
     print(puzzle2slow(sample_input))
---
+++ AdventofCode2020/day2.py
@@ -0,0 +1,40 @@
+def puzzle3(input):
+    input_list = [x for x in input.split("\n") if x]
+    valid_password_count = 0
+    for row in input_list:
+        nums, letter_colon, password = row.split()
+        num1, num2 = nums.split("-")
+        num1, num2 = int(num1), int(num2)
+        letter = letter_colon[0]
+        if num1 <= password.count(letter) <= num2:
+            valid_password_count += 1
+    return valid_password_count
+
+
+def puzzle4(input):
+    input_list = [x for x in input.split("\n") if x]
+    valid_password_count = 0
+    for row in input_list:
+        nums, letter_colon, password = row.split()
+        num1, num2 = nums.split("-")
+        num1, num2 = int(num1), int(num2)
+        letter = letter_colon[0]
+
+        valid_position_1 = password[num1 - 1] == letter
+        valid_position_2 = password[num2 - 1] == letter
+        if valid_position_1 ^ valid_position_2:
+            valid_password_count += 1
+    return valid_password_count
+
+
+sample_input2 = "1-3 a: abcde\n1-3 b: cdefg\n2-9 c: ccccccccc"
+
+input2 = open("input2.txt", "r").read()
+
+
+if __name__ == "__main__":
+    print(puzzle3(sample_input2))
+    print(puzzle3(input2))
+
+    print(puzzle4(sample_input2))
+    print(puzzle4(input2))
