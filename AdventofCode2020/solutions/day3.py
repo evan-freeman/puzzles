9505477e0be3bdaabd44032c43e4b02955858a02
evan-freeman <59422750+evan-freeman@users.noreply.github.com>
2020-12-03 16:39:36 -0800

    inits, day3

--- AdventofCode2020/solutions/day3.py
+++ AdventofCode2020/solutions/day3.py
@@ -7,13 +7,16 @@ def vector_addition(x, y):
     return x
 
 
-def puzzle5(input, position=[0, 0], slope=[3, 1]):
-    hill = [x for x in input.split("\n") if x]
+def puzzle5(input, slope=[3, 1], clean_input=True, hill=None):
+    # Flag to avoid cleaning multiple times in puzzle 6
+    if clean_input:
+        hill = [x for x in input.split("\n") if x]
+    position = [0, 0]
     tree_count = 0
     hill_width = len(hill[0])
     hill_height = len(hill)
     while position[1] < hill_height:
-        x = (position[0]) % hill_width
+        x = position[0] % hill_width
         y = position[1]
         tree_count += hill[y][x] == "#"
         position = vector_addition(position, slope)
@@ -21,16 +24,22 @@ def puzzle5(input, position=[0, 0], slope=[3, 1]):
 
 
 def puzzle6(input):
-    pass
+    hill = [x for x in input.split("\n") if x]
+    slope_list = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
+    answers = []
+    for slope in slope_list:
+        answers.append(puzzle5(input, slope=slope, clean_input=False, hill=hill))
+    output = 1
+    for answer in answers:
+        output *= answer
+    return output
 
 
 if __name__ == "__main__":
     sample_input3 = "..##.......\n#...#...#..\n.#....#..#.\n..#.#...#.#\n.#...##..#.\n..#.##.....\n.#.#.#....#\n.#........#\n#.##...#...\n#...##....#\n.#..#...#.#"
-
     input3 = open("inputs/input3.txt", "r").read()
 
-    # print(puzzle5(sample_input3))
+    print(puzzle5(sample_input3))
     print(puzzle5(input3))
-
-    # print(puzzle6(sample_input3))
-    # print(puzzle6(input3))
+    print(puzzle6(sample_input3))
+    print(puzzle6(input3))
